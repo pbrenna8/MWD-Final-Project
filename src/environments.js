cf0f100b2ed7e88a9ad03632ece9dd5d0b7b3b1d
Patrick J. Brennan <pjbrennan@patricksmbp3782.dhcp.nd.edu>
2022-03-01 16:31:51 -0500

    Initial Commit

---
+++ src/environments.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: "6kbly09HmS1v17AfZzjZzEZ278YsPmOGyqIfh3Xn",
+    JAVASCRIPT_KEY: "mT7RbTNW9iZCkvGFU2c8kIAKHIvOXXNlEp09q7IA",
+    SERVER_URL: "https://parseapi.back4app.com/"
+};
